7d0c6773e9b2dd1f7eb355ed111bc18485ffd7ef
durosly <slyboydon1@gmail.com>
2023-12-09 02:54:43 +0100

    ✅add copy data to clipboard

--- app/(public)/auth/user/orders/__components/show-details-btn.jsx
+++ app/(public)/auth/user/orders/__components/show-details-btn.jsx
@@ -1,8 +1,12 @@
 "use client";
 import { SlExclamation } from "react-icons/sl";
 import Markdown from "react-markdown";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { IoCopyOutline } from "react-icons/io5";
+import toast from "react-hot-toast";
+import { Fragment } from "react";
 
-function ShowDetailsBtn({ id, details, items }) {
+function ShowDetailsBtn({ id, items }) {
 	return (
 		<>
 			<button
@@ -21,19 +25,60 @@ function ShowDetailsBtn({ id, details, items }) {
 			>
 				<div className="modal-box">
 					<h3 className="font-bold text-lg mb-2">Service details</h3>
+
 					{items && items.length > 0 ? (
 						items.map((item) => (
-							<>
+							<Fragment key={JSON.stringify(item)}>
 								<div className="divider">Info</div>
 
-								<div className="prose">
-									<pre>{item.info}</pre>
+								<div className="">
+									{/* <pre>{item.info}</pre> */}
+									<div className="space-y-2">
+										{item.info
+											.split(/\s+/)
+											.map((data, i) => {
+												if (data)
+													return (
+														<p
+															key={i}
+															className="space-x-1"
+														>
+															<span>{data}</span>
+															<CopyToClipboard
+																text={`Nice`}
+																options={{
+																	message:
+																		"Copied to clipboard",
+																}}
+																onCopy={(
+																	text,
+																	result
+																) => {
+																	if (
+																		result
+																	) {
+																		return toast(
+																			"copied"
+																		);
+																	}
+																}}
+															>
+																<button className="btn btn-xs btn-square">
+																	<IoCopyOutline />
+																</button>
+															</CopyToClipboard>
+														</p>
+													);
+											})}
+									</div>
 									<p className="font-bold my-5">
 										Instructions
 									</p>
-									<Markdown>{item.instruction}</Markdown>
+									<div className="prose">
+										<Markdown>{item.instruction}</Markdown>
+									</div>
 								</div>
-							</>
+							</Fragment>
 						))
 					) : (
 						<p>No item</p>
