55e1f8d72c44de67c30413a53830231aff07b86b
durosly <slyboydon1@gmail.com>
2023-12-12 17:23:53 +0100

    Update transactions.js

--- models/transactions.js
+++ models/transactions.js
@@ -1,10 +1,18 @@
 import mongoose from "mongoose";
 import paginate from "mongoose-paginate-v2";
+import { uuid } from "uuidv4";
 // import referralCodeGenerator from 'referral-code-generator';
 
 const transactionSchema = new mongoose.Schema({
 	userEmail: { type: String },
 	transactionId: String,
+	ownRef: { type: String, default: () => uuid() },
+	type: { type: String, enum: ["reg", "crypto"], default: "reg" },
+	status: {
+		type: String,
+		enum: ["pending", "completed"],
+		default: "pending",
+	},
 	amount: { type: Number },
 	createdAt: { type: Date, default: Date.now },
 });
