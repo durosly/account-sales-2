b4c3c41d585233b42b9c6fb676e049a1038fa656
durosly <slyboydon1@gmail.com>
2023-12-12 17:23:43 +0100

    ✅add links to crypto and cash payment pages

--- app/(public)/auth/user/funds/page.jsx
+++ app/(public)/auth/user/funds/page.jsx
@@ -1,17 +1,30 @@
-import FundAccountForm from "./__components/fund-account-form";
-import { getServerSession } from "next-auth";
-import { options } from "@/auth/options";
+import Link from "next/link";
+import { IoMdCard } from "react-icons/io";
+import { FaBitcoin } from "react-icons/fa";
 
 async function AddFundsPage() {
-	const session = await getServerSession(options);
-
 	return (
 		<>
 			<div className="px-10 mb-20 mt-5">
 				<h1 className="text-4xl font-bold text-center mb-10">
 					Add funds to account
 				</h1>
-				<FundAccountForm user={session?.user} />
+				<div className="max-w-md mx-auto space-y-5">
+					<Link
+						href="/auth/user/funds/cash"
+						className="btn btn-primary btn-block"
+					>
+						<IoMdCard />
+						Card, transfer, USSD, etc
+					</Link>
+					<Link
+						href="/auth/user/funds/crypto"
+						className="btn btn-primary btn-block"
+					>
+						<FaBitcoin />
+						Crypto
+					</Link>
+				</div>
 			</div>
 		</>
 	);
