1e1f5fd9d35aa766d8d4ec61e3e694c1f906626e
durosly <slyboydon1@gmail.com>
2023-12-13 12:33:58 +0100

    Update page.jsx

--- app/(public)/auth/page.jsx
+++ app/(public)/auth/page.jsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 function AuthPage() {
 	return (
 		<div className="px-10">
-			<div className="flex max-sm:flex-col max-w-md max-sm:h-[30rem] h-[30rem] mx-auto bg-base-200 rounded-2xl overflow-hidden my-20">
+			<div className="flex max-sm:flex-col max-w-md max-sm:h-[35rem] h-[30rem] mx-auto bg-base-200 rounded-2xl overflow-hidden my-20">
 				<ToggleForms />
 				<div className="flex-1 relative transition-all rounded-r-2xl max-sm:peer-checked:max-h-[5rem] max-sm:max-h-[100%] sm:peer-checked:max-w-[5rem] sm:max-w-[100%] p-10 bg-primary text-white">
 					<Link
@@ -25,7 +25,7 @@ function AuthPage() {
 					<Link
 						href="/auth?action=signup"
 						id="show-signup"
-						className="bg-primary text-center text-white text-sm py-1 !rounded-xl max-sm:!rounded-t-none sm:!rounded-b-none sm:rotate-90 absolute left-1/2 sm:left-0 top-5 sm:top-1/2 max-sm:-translate-x-1/2 sm:-translate-y-1/2"
+						className="bg-primary text-center text-white text-sm max-sm:p-3 !rounded-xl max-sm:!rounded-t-none sm:!rounded-b-none sm:rotate-90 absolute left-1/2 sm:left-0 top-5 sm:top-1/2 max-sm:-translate-x-1/2 sm:-translate-y-1/2"
 					>
 						Show Signup
 					</Link>
