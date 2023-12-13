1058fbb7cc49c39b15c213d40b529a3b6a9e8f76
durosly <slyboydon1@gmail.com>
2023-12-13 17:23:51 +0100

    🗑️remove loading update

--- app/(public)/auth/user/loading.jsx
+++ app/(public)/auth/user/loading.jsx
@@ -5,14 +5,7 @@ function loading() {
 		<div className="flex min-h-[500px] justify-center items-center">
 			<div className="text-center">
 				<h3 className="text-xl font-bold">AccHub</h3>
-				<div class="wrapper">
-					<div class="circle"></div>
-					<div class="circle"></div>
-					<div class="circle"></div>
-					<div class="shadow"></div>
-					<div class="shadow"></div>
-					<div class="shadow"></div>
-				</div>
+				<span className="loading loading-dots"></span>
 			</div>
 		</div>
 	);
