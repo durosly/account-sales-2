fb01f4fb0d14ce48d3e009139890c13b43afdea3
durosly <slyboydon1@gmail.com>
2023-12-14 17:35:06 +0100

    Update page.jsx

--- app/(public)/auth/forgot-password/sent/page.jsx
+++ app/(public)/auth/forgot-password/sent/page.jsx
@@ -3,7 +3,7 @@ import { BsCheckLg } from "react-icons/bs";
 
 function ForgotPasswordEmailSentPage() {
 	return (
-		<div className="min-h-screen flex justify-center items-center py-10 px-5 ">
+		<div className="min-h-[500px] flex justify-center items-center py-10 px-5 ">
 			<div className="max-w-sm flex flex-col items-center gap-10 rounded-xl bg-white p-5 border">
 				<div className="w-14 flex justify-center items-center aspect-square rounded-full bg-success/20">
 					<BsCheckLg className="w-10 h-10 stroke-current" />
