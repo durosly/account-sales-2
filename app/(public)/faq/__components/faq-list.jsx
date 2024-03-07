f915f5f2d2f251342041a900b481138fd2454ec1
durosly <slyboydon1@gmail.com>
2024-03-07 09:08:18 +0100

    Update faq-list.jsx

--- app/(public)/faq/__components/faq-list.jsx
+++ app/(public)/faq/__components/faq-list.jsx
@@ -18,9 +18,10 @@ function FaqList() {
 								/>
 							</Disclosure.Button>
 							<Disclosure.Panel className="px-4 pb-2 pt-4 text-sm text-gray-500">
-								If you're unhappy with your purchase for any
-								reason, email us within 90 days and we'll refund
-								you in full, no questions asked.
+								If you&apos;re unhappy with your purchase for
+								any reason, email us within 90 days and
+								we&apos;&aposll refund you in full, no questions
+								asked.
 							</Disclosure.Panel>
 						</>
 					)}
